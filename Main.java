8facb878e559622088ebe6a358e7f96b2155f362
Amgalan <darzhaev@gmail.com>
2021-10-14 23:44:10 +0800

    Divided code to different files

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        Operation op = null;
+        Scanner in = new Scanner(System.in);
+        int num1 = in.nextInt();
+        char Operation1 = in.next().charAt(0);
+        int num2 = in.nextInt();
+        if (Operation1 == '+')
+            op = Operation.SUM;
+        else if (Operation1 == '-'){
+            op = Operation.SUBTRACT;
+        }
+        else if (Operation1 == '/'){
+            op = Operation.DIVIDE;
+        }
+        else if (Operation1 == '*'){
+            op = Operation.MULTIPLY;
+        }
+        */
+
+/*        System.out.println(op.action(num1, num2));
+        in.close();
+
+ */
+        roman_numerals z = roman_numerals.valueOf("X");
+        System.out.println(z.getCode_to_roman());
+
+    }
+}
